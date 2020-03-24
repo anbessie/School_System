e96cbbb916d310435df108a146d4e68e3504bc5d
edna <edna@LAPTOP-EAKLB1S5>
2020-03-24 16:38:10 -0400

    First Commit...

---
+++ src/test/java/com/adginfosys/shoping/SchoolSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adginfosys.shoping;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchoolSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
